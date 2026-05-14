c2d834b69d7278359e68c5737a54b73a27cc27fb
Starshi <164260057+Starshii@users.noreply.github.com>
2026-05-14 21:04:24 +0200

    first attempt at the project pages
    BUT ITS NOT MOBILE COMPATIBLE HAHAHHAHAHAHAHAHAHAHAHAHAHAHHAHAHAHAHAHAHAHAHAHAHAHAHA

---
+++ index.js
@@ -0,0 +1,7 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/nARRg0FA3Qf5fWU1/scene.splinecode');
+
+
