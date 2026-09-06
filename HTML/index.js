0d01007990885c6c765c9aa5639ea62579d2294a
Starshi <164260057+Starshii@users.noreply.github.com>
2026-09-06 15:19:17 +0100

    Delete HTML directory
    oopsie uploaded the entire map

--- HTML/index.js
+++
@@ -1,7 +0,0 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/nARRg0FA3Qf5fWU1/scene.splinecode');
-
-
